d863c13c6a43954bc7a1e634c7120e5c847c37e9
KimYeongChan <goldfish2679@naver.com>
2022-10-06 14:22:06 +0900

    django

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.views.decorators.csrf import csrf_exempt
 import random
 # Create your views here.
+nextid=4
 topics = [
     {'id':1, 'title':'routing','body':'routing is...'},
     {'id':2, 'title':'view','body':'view is...'},
     {'id':3, 'title':'model','body':'model is...'}
 ]
+
+
 def HTMLtemplate(articleTag):
     global topics
     ol=''
@@ -20,9 +24,9 @@ def HTMLtemplate(articleTag):
         </ol>
         {articleTag}
         <ul>
-        <li></li>
-        <li><a href = "/create">create</a></li>
-        <li></li>
+        <li><a href="">update</a></li>
+        <li><a href = "/create/">create</a></li>
+        <li><a href="">delete</a></li>
         </ul>
     </body>    
     </html>
@@ -35,8 +39,28 @@ def index(request):
     '''
     return HttpResponse(HTMLtemplate(article))
 
+@csrf_exempt
 def create(request):
-    return HttpResponse('Welcome create page!')
+    
+    if request.method=='GET':
+        article='''
+        <form action="/create/" method="POST">
+            <p><input type="text" name="title" placeholder="Enter the title.."></p>
+            <p><textarea name="body" placeholder="body"></textarea></p>
+            <p><input type="submit" value="제출!"></p>
+        </form>
+        '''
+        return HttpResponse(HTMLtemplate(article))
+    elif request.method=='POST':
+        global nextid
+        title=request.POST['title'] # request.POST는 FORM태그에서 전송한 데이터를 딕셔너리형태로 저장하고 있음.
+        body=request.POST['body']# flask의 request.form과 같은 뜻임.
+        topics.append({'id':nextid,'title':title,'body':body})
+        url='/read/'+str(nextid)
+        nextid+=1
+        return redirect(url)
+    
+    
 
 def read(request, id):
     global topics
